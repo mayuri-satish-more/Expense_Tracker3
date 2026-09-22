d5784da28e6b5556250c744b445ba16561260c43
mayuri-satish-more <mayurimore2345m@gmail.com>
2026-09-23 01:37:53 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://expense-tracker3-backend.onrender.com",
+  baseURL: "https://expense-tracker3-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
